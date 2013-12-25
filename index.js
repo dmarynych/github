9f6ee4c72d908270cb0ec4d04fb8397da94eabd6
stark <dmarynych@gmail.com>
2013-12-26 00:55:24 +0200

    conditional req

--- index.js
+++ index.js
@@ -1,38 +1,93 @@
-var https = require('https');
+var https = require('https'),
+    redis = require("redis"),
+    client = redis.createClient();
 
 var Github = function () {
     this.token = '';
 
 
     this.query = function (path, params, callback) {
+        // first, check cache
+        client.hget('ghcache', path, function (err, res) {
+            console.log('res', res);
+
+            if(!res) {
+                console.log('do query');
+                this.httpQuery(path, params, function (res) {
+                    client.hset('ghcache', path, JSON.stringify(res), function () {
+                        callback(res);
+                    });
+                });
+            }
+            else {
+                console.log('cache');
+                var cachedVal = JSON.parse(res);
+                console.log(cachedVal);
+                params.etag = cachedVal.headers.etag;
+                
+                this.httpQuery(path, params, function (res) {
+                    console.log('done');
+                    if(res.statusCode === 304) {
+                        console.log('etag cache');
+
+                        callback(cachedVal);
+                    }
+                    else {
+                        console.log('cache invalideted');
+                        callback(res);
+                    }
+
+
+                    client.hset('ghcache', path, JSON.stringify(res));
+                });
+                //callback(obj);
+            }
+
+        }.bind(this));
+    };
+
+    this.httpQuery = function (path, params, callback) {
         var options = {
             hostname: 'api.github.com',
             port: 443,
             path: path,
             headers: {
-                'User-Agent': 'nodejs',
-                'If-None-Match': '"de8ea5d318a97df871f1c310cc14a048"'
+                'User-Agent': 'nodejs'
+                //'If-None-Match': '"de8ea5d318a97df871f1c310cc14a048"'
             }
         };
+        if(params.etag) {
+            options.headers['If-None-Match'] = params.etag;
+        }
+
         console.log(options);
         https.get(options, function(res) {
             console.log(res.statusCode);
 
-            res.on('data', function (chunk) {
-                callback(chunk.toString('utf8'), res.headers);
-            });
-
+            if(res.statusCode === 304) {
+                callback({
+                    statusCode: res.statusCode
+                });
+            }
+            else {
+                res.on('data', function (chunk) {
+                    callback({
+                        statusCode: res.statusCode,
+                        body: chunk.toString('utf8'),
+                        headers: res.headers
+                    });
+                });
+            }
 
         }).on('error', function(e) {
-            console.log("Got error: " + e.message);
-        });
+                console.log("Got error: " + e.message);
+            });
     }
 };
 
 
 var github = new Github();
 
-github.query('/users/dmarynych', {}, function (res, headers) {
-    console.log(res.statusCode);
-    console.log(headers);
+github.query('/users/dmarynych', {}, function (res) {
+    console.log(res);
 });
